2e49d2a9ad12d026dcccb21f6942ca5d8f272798
Wasabi5392 <taka.dd.sdr1729@gmail.com>
2022-12-01 20:44:19 +0900

    バリデーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+.[a-z]+\z/i.freeze
+  NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+  KANA_REGEX = /\A[ァ-ヶ]+\z/
+
+  validates :email, presence: true, uniqueness: true, format: { with: EMAIL_REGEX }
+  validates :encrypted_password, presence: true, length: { minimum: 6 }, format: { with: PASSWORD_REGEX }, message: 'には英字と数字の両方を含めて設定してください'
+  validates :nickname, presence: true
+  validates :firstname_in_kanji, presence: true, format: { with: NAME_REGEX }
+  validates :lastname_in_kanji, presence: true, format: { with: NAME_REGEX }
+  validates :firstname_in_kana, presence: true, format: { with: KANA_REGEX }
+  validates :lastname_in_kana, presence: true, format: { with: KANA_REGEX }
+  validates :birthday, presence: true
 end
