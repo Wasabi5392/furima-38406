35733858a1d5bf7298d13ffdc44cf6a4035ffda3
Wasabi5392 <taka.dd.sdr1729@gmail.com>
2022-12-02 17:19:20 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+.[a-z]+\z/i.freeze
-  NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
-  KANA_REGEX = /\A[ァ-ヶ]+\z/
+  NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
+  KANA_REGEX = /\A[ァ-ヶ]+\z/.freeze
 
-  validates :email, presence: true, uniqueness: true, format: { with: EMAIL_REGEX }
-  validates :encrypted_password, presence: true, length: { minimum: 6 }, format: { with: PASSWORD_REGEX }
+
+  validates :encrypted_password, format: { with: PASSWORD_REGEX }
   validates :nickname, presence: true
   validates :firstname_in_kanji, presence: true, format: { with: NAME_REGEX }
   validates :lastname_in_kanji, presence: true, format: { with: NAME_REGEX }
