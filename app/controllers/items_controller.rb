0e53a78c67e14365d4d53acf153f8ef068b888ac
Wasabi5392 <taka.dd.sdr1729@gmail.com>
2022-12-12 14:28:46 +0900

    出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,31 @@
 class ItemsController < ApplicationController
+  before_action :login_check, only: [:new]
+  def index
+    @prototypes = Prototype.all
+  end
   def index
   end
 
   def new
     @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :image, :category_id, :status_id, :information, :price, :prefecture_id, :scheduled_delivery_id, :delivery_charge_id).merge(user_id: current_user.id)
+  end
+
+  def login_check
+    authenticate_user!
   end
 end
