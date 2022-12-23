6d42923a9f90ef82380d9c747fdf1f29fed01ee6
Wasabi5392 <taka.dd.sdr1729@gmail.com>
2022-12-23 18:27:54 +0900

    viewの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :login_check, only: [:new]
+  before_action :login_check, only: [:new, :edit]
   def index
     @items = Item.order("created_at DESC")
   end
@@ -19,6 +19,23 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+    @item.update(item_params)
   end
 
   private
